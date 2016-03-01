66c9a47ca720c5b533bb85a872bc20647e7148bd
Ricardo <ricardo.b.silva13@gmail.com>
2016-03-01 20:05:53 -0300

    exemplo if/else e variaveis

---
+++ exercicios_php/if_else.php
@@ -0,0 +1,11 @@
+<?php
+	$numero1 = 30;
+	$numero2 = 40;
+
+	if ($numero1 > $numero2) {
+		echo $numero1;
+	} else {
+		echo $numero2;
+	}
+
+?>
---
+++ exercicios_php/variaveis.php
@@ -0,0 +1,8 @@
+<?php
+	
+	$numero1 = 10.10;
+	$numero2 = '10';
+
+	$resultado = $numero1 . $numero2;
+	echo $resultado;
+?>
