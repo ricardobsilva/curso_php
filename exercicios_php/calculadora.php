c36dc98a4483add07fd45fee25a44983ab3ce488
Ricardo <ricardo.b.silva13@gmail.com>
2016-03-01 21:45:20 -0300

    functions

---
+++ exercicios_php/calculadora.php
@@ -0,0 +1,11 @@
+<?php
+
+include('functions.php');
+
+$n1 = 10;
+$n2 = 15;
+
+$result  =  somar($n1 , $n2);
+echo $result;
+
+?>
---
+++ exercicios_php/functions.php
@@ -0,0 +1,9 @@
+<?php
+	
+	function somar($param1 , $param2){
+		$resultado = $param1 + $param2;
+		return $resultado;
+	}
+
+	
+?>
